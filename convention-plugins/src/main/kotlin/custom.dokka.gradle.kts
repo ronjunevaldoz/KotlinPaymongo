8346e74f12f510b3b07f1359b04e72178dbe12b3
ronjunevaldoz <ronjune.lopez@gmail.com>
2024-09-24 00:15:51 +0800

    remove dokka

--- convention-plugins/src/main/kotlin/custom.dokka.gradle.kts
+++ convention-plugins/src/main/kotlin/custom.dokka.gradle.kts
@@ -1,24 +1,19 @@
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.kotlin.dsl.`maven-publish`
-import org.gradle.kotlin.dsl.version
-
-plugins {
-    id("org.jetbrains.dokka")
-}
-
-val dokkaOutputDir = "$buildDir/dokka"
-
-tasks.getByName<org.jetbrains.dokka.gradle.DokkaTask>("dokkaHtml") {
-    outputDirectory.set(file(dokkaOutputDir))
-}
-
-val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
-    delete(dokkaOutputDir)
-}
-
-val javadocJar = tasks.register<Jar>("javadocJar") {
-    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
-    archiveClassifier.set("javadoc")
-    from(dokkaOutputDir)
-}
+//plugins {
+//    id("org.jetbrains.dokka")
+//}
+//
+//val dokkaOutputDir = "$buildDir/dokka"
+//
+//tasks.getByName<org.jetbrains.dokka.gradle.DokkaTask>("dokkaHtml") {
+//    outputDirectory.set(file(dokkaOutputDir))
+//}
+//
+//val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+//    delete(dokkaOutputDir)
+//}
+//
+//val javadocJar = tasks.register<Jar>("javadocJar") {
+//    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+//    archiveClassifier.set("javadoc")
+//    from(dokkaOutputDir)
+//}
--- convention-plugins/src/main/kotlin/module.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/module.publication.gradle.kts
@@ -1,7 +1,6 @@
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.`maven-publish`
-import java.util.Properties
 
 plugins {
     `maven-publish`
@@ -20,7 +19,7 @@ publishing {
         // Provide artifacts information required by Maven Central
         pom {
             name.set("KotlinPaymongo")
-            description.set("Paymongo client for kotlin")
+            description.set("Paymongo Kotlin Client")
             licenses {
                 license {
                     name.set("MIT")
@@ -48,36 +47,7 @@ publishing {
     }
 }
 
-
-val properties = Properties().apply {
-    load(rootProject.file("secret.properties").reader())
-}
-
-// <OSSRH jira account username>
-val ossrhUsername = properties["ossrhUsername"] as String?
-// <OSSRH jira account password>
-val ossrhPassword  = properties["ossrhPassword"] as String?
-// <your-key-id-(last 8 character of your key)>
-val signingKeyId = properties["signing.keyId"] as String?
-// passphrase
-val signingPassword = properties["signing.password"] as String?
-// gpg --export-secret-keys <last 8 character of your key> | base64
-val signingKey = properties["signing.secretKey"] as String?
-// gpg --edit-key F8F55D3E
-// gpg --list-signatures
-// gpg --keyring secring.gpg --export-secret-keys > ~/.gnupg/secring.gpg
-// or raw base64 secretkey
-// gpg --export-secret-keys <last 8 character of your key> | base64
-// https://stackoverflow.com/a/39573795/2801777
-
-
 signing {
-    useInMemoryPgpKeys(
-        signingKeyId,
-        signingKey,
-        signingPassword
-    )
-
     if (project.hasProperty("signing.gnupg.keyName")) {
         useGpgCmd()
         sign(publishing.publications)
